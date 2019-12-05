879b3fd1a0faccb3e5d424c2cc66fdd4e74e5ef7
sailaja <sailaja+1@sunfra.com>
2019-12-05 14:38:24 +0530

    @Mohan:Updated New maven project

---
+++ src/main/java/com/sunfra/verticals/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.sunfra.verticals.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sunfra/verticals/mavenproject/FirstJenkin.java
@@ -0,0 +1,20 @@
+package com.sunfra.verticals.mavenproject;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstJenkin {
+
+	@Test
+	public void testJenkin() throws InterruptedException {
+
+		/*System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		System.out.println("Hello, World");*/
+		
+		System.out.println("First maven project with Jenkins");
+	}
+}
